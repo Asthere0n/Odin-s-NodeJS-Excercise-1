137a43bfa1b89d1e5e6abbf9d8866e8baa32b6e4
Asthere0n <cristiansanchezsonido@gmail.com>
2025-02-28 15:39:55 +0000

    Our server is serving index and 404

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+// Creation of the server
 const app = express()
 
+// Setting up EJS to manage our views
+app.set('view engine', 'ejs')
+app.set('views', './models')
+
+// 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const server = app.listen(8080, ()=>{
+    const port = server.address().port
+    console.log('Connection stablished')
+    console.log(`URL: http://localhost:${port}`)
+})
+
+// GET Request
+
+app.get('/', (req, res)=>{
+    console.log('request received')
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact-me', (req, res) => {
+    res.render('contact-me')
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('404.html', { root: __dirname })
+})
